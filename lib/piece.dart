b971993572bef3237b963dc14b21d17d74665615
unknown <rajakhushi13@gmail.com>
2025-03-16 19:40:39 +0530

    Only valid move

--- lib/piece.dart
+++ lib/piece.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tetris_game/game_board.dart';
 import 'package:tetris_game/values.dart';
 
 class Piece {
@@ -19,39 +20,25 @@ class Piece {
   void initializePiece() {
     switch (type) {
       case Tetromino.L:
-        position = [
-          -26, -16, -6, -5
-        ];
+        position = [-26, -16, -6, -5];
         break;
       case Tetromino.J:
-        position = [
-          -25, -15, -5, -6
-        ];
+        position = [-25, -15, -5, -6];
         break;
       case Tetromino.I:
-        position = [
-          -4, -5, -6, -7
-        ];
+        position = [-4, -5, -6, -7];
         break;
       case Tetromino.O:
-        position = [
-          -15, -16, -5, -6
-        ];
+        position = [-15, -16, -5, -6];
         break;
       case Tetromino.S:
-        position = [
-          -15, -14, -6, -5
-        ];
+        position = [-15, -14, -6, -5];
         break;
       case Tetromino.Z:
-        position = [
-          -17, -16, -6, -5
-        ];
+        position = [-17, -16, -6, -5];
         break;
       case Tetromino.T:
-        position = [
-          -26, -16, -6, -15
-        ];
+        position = [-26, -16, -6, -15];
         break;
     }
   }
@@ -60,17 +47,17 @@ class Piece {
   void movePiece(Direction direction) {
     switch (direction) {
       case Direction.down:
-        for(int i=0; i < position.length; i++) {
+        for (int i = 0; i < position.length; i++) {
           position[i] += rowLength;
         }
         break;
       case Direction.left:
-        for(int i=0; i < position.length; i++) {
+        for (int i = 0; i < position.length; i++) {
           position[i] -= 1;
         }
         break;
       case Direction.right:
-        for(int i=0; i < position.length; i++) {
+        for (int i = 0; i < position.length; i++) {
           position[i] += 1;
         }
         break;
@@ -80,6 +67,7 @@ class Piece {
 
   // rotate piece
   int rotationState = 1;
+
   void rotatePiece() {
     // new position
     List<int> newPosition = [];
@@ -96,62 +84,74 @@ class Piece {
               position[1] + rowLength,
               position[1] + rowLength + 1
             ];
-            // update position
-            position = newPosition;
-            // update rotation state
-            rotationState = (rotationState + 1) % 4;
+            // check that this new move is valid move before assigning it to the real position
+            if(piecePositionIsValid(newPosition)) {
+              // update position
+              position = newPosition;
+              // update rotation state
+              rotationState = (rotationState + 1) % 4;
+            }
             break;
           case 1:
             /*
             o o o
             o
             */
-          // get the new position
+            // get the new position
             newPosition = [
               position[1] - 1,
               position[1],
               position[1] + 1,
               position[1] + rowLength - 1
             ];
-            // update position
-            position = newPosition;
-            // update rotation state
-            rotationState = (rotationState + 1) % 4;
+            // check that this new move is valid move before assigning it to the real position
+            if(piecePositionIsValid(newPosition)) {
+              // update position
+              position = newPosition;
+              // update rotation state
+              rotationState = (rotationState + 1) % 4;
+            }
             break;
           case 2:
-          /*
+            /*
             o o
               o
               o
             */
-          // get the new position
+            // get the new position
             newPosition = [
               position[1] + rowLength,
               position[1],
               position[1] - rowLength,
               position[1] - rowLength - 1
             ];
-            // update position
-            position = newPosition;
-            // update rotation state
-            rotationState = (rotationState + 1) % 4;
+            // check that this new move is valid move before assigning it to the real position
+            if(piecePositionIsValid(newPosition)) {
+              // update position
+              position = newPosition;
+              // update rotation state
+              rotationState = (rotationState + 1) % 4;
+            }
             break;
           case 3:
-          /*
+            /*
                 o
             o o o
             */
-          // get the new position
+            // get the new position
             newPosition = [
               position[1] - rowLength + 1,
               position[1],
               position[1] + 1,
               position[1] - 1
             ];
-            // update position
-            position = newPosition;
-            // update rotation state
-            rotationState = (rotationState + 1) % 4;
+            // check that this new move is valid move before assigning it to the real position
+            if(piecePositionIsValid(newPosition)) {
+              // update position
+              position = newPosition;
+              // update rotation state
+              rotationState = (rotationState + 1) % 4;
+            }
             break;
         }
         break;
@@ -175,4 +175,47 @@ class Piece {
         throw UnimplementedError();
     }
   }
+
+  // check if valid position
+  bool positionIsValid(int position) {
+    // get the row and col of position
+    int row = (position / rowLength).floor();
+    int col = position % rowLength;
+
+    // if the position is taken, return false
+    if (row < 0 || col < 0 || gameBoard[row][col] != null) {
+      return false;
+    }
+
+    // otherwise position is valid so return true
+    else {
+      return true;
+    }
+  }
+
+  // check if piece is valid position
+bool piecePositionIsValid(List<int> piecePosition) {
+    bool firstColOccupied = false;
+    bool lastColOccupied = false;
+
+    for(int pos in piecePosition) {
+      // return false if any position is already taken
+      if(!positionIsValid(pos)) {
+        return false;
+      }
+
+      // get the col of position
+      int col = pos % rowLength;
+
+      // check if the first or last column is occupied
+      if(col == 0) {
+        firstColOccupied = true;
+      }
+      if(col == rowLength - 1) {
+        lastColOccupied = true;
+      }
+    }
+    // if there is a piece in the first col and last col, it is going through the wall
+  return !(firstColOccupied && lastColOccupied);
+}
 }
